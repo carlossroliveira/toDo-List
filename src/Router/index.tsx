01fdb4dc31c23a49a16b166ff0663dde1d7ce83a
Carlos Oliveira <carlos.sroliveira@hotmail.com>
2023-05-07 23:42:58 -0300

    Ajustes nas rotas

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -3,14 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // Components
 import { Layout } from '../components/Layout'
+import { Content } from '../components/Content'
 
 export const DefaultRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path="/" element={<h1>Paga 1</h1>} />
-          <Route path="/page" element={<h1>Paga 2</h1>} />
+          <Route path="/" element={<Content />} />
         </Route>
 
         <Route path="*" element={<h1>Error</h1>} />
