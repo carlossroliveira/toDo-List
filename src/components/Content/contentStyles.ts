ed0bf83a2d7da64ec535101886ba274fa32a8c6d
Carlos Oliveira <carlos.sroliveira@hotmail.com>
2023-05-07 23:42:05 -0300

    Criação do componente Content

---
+++ src/components/Content/contentStyles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const ContainerSC = styled.section`
+  margin: 0 auto;
+  padding: 0 2rem;
+  max-width: 70rem;
+
+  grid-area: main;
+`
---
+++ src/components/Content/index.tsx
@@ -0,0 +1,16 @@
+// Components
+import { Input } from '../Input'
+import { Table } from '../Table'
+
+// Styles
+import { ContainerSC } from './contentStyles'
+
+export const Content = () => {
+  return (
+    <ContainerSC>
+      <Input />
+
+      <Table />
+    </ContainerSC>
+  )
+}
